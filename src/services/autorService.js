1929cb64340bde7c946dcc848dd9ed5e7de81fd3
EduardoZenDev <lalozen68@gmail.com>
2025-06-27 13:26:38 -0600

    first commit2

--- src/services/autorService.js
+++ src/services/autorService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://autoreslibros.somee.com/api/Autor'; // Cambia el puerto según tu backend
+const API_BASE = 'https://tienda-microservicios-autor-api2.onrender.com/api/Autor'; // Cambia el puerto según tu backend
 
 // Obtener todos los autores
 export const obtenerAutores = () => axios.get(API_BASE);
--- src/services/libroService.js
+++ src/services/libroService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://librosservice.somee.com/api/LibroMaterial';
+const BASE_URL = 'https://uttt-micro-libro.onrender.com/api/LibroMaterial';
 
 
 
