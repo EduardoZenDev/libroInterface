a894a8626ff9a9279a1584cb38c166b6727d7d91
EduardoZenDev <lalozen68@gmail.com>
2025-07-18 13:08:42 -0600

    version examen v2

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ const App = ({ onLogout }) => {
 
             <button
               className="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded transition duration-200"
-              onClick={() => navigate('/vistaNueva')}
+              onClick={() => navigate('/articulos')}
             >
               🔍 Vista Nueva
             </button>
